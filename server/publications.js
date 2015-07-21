eb18d5b762aa4702a53080cfce136c07a643490b
Dustin Coates <dustin@dcoates.com>
2015-07-21 12:30:55 -0400

    Add Resources

--- server/publications.js
+++ server/publications.js
@@ -2,3 +2,8 @@ Meteor.publish('board', function(id){
   check(id, String);
   return Boards.find(id);
 });
+
+Meteor.publish('resources', function(boardId){
+  check(boardId, String);
+  return Resources.find({boardId: boardId});
+});
