48b27d92506bf07605b7421c39badfa80de78a9f
Dustin Coates <dustin@dcoates.com>
2015-08-21 15:06:59 -0400

    Notification improvements

--- client/templates/resources/resource.js
+++ client/templates/resources/resource.js
@@ -1,12 +1,24 @@
 Template.resource.helpers({
   isCode: function(type){
-    return type === "code";
+    return type === 'code';
   },
   isURL: function(type){
-    return type === "url";
+    return type === 'url';
   }
 });
 
 Template.resource.onRendered(function(){
   Prism.highlightAll();
+
+  if(this.data.userId !== Meteor.userId() && secondsAgo(this.data.submitted) < 90){
+    sendNotifications('New Post', {
+      body: this.data.content,
+      icon: '/images/notification.png',
+      tag: this.data.boardId
+    });
+  }
 });
+
+var secondsAgo = function(submittedTime){
+  return (new Date() - submittedTime) / 1000;
+}
